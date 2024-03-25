d397094a8b9855bcb7c9544698f95891682be342
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-03-25 10:52:03 +0100

    .

--- home.js
+++ home.js
@@ -1,6 +1,25 @@
-console.log('hello Mbanefo')
+/*console.log('hello Mbanefo')
 if(50>151){
     console.log('gio')
 }
 else{console.log('bio')
 }
+let firstName= ("obiajulu");
+let lastName= ("dibiaezue");
+let fullName= (firstName+lastName);
+console.log(fullName)
+*/
+let username= window.prompt('whats your username');
+console.log(username);
+let fullName= window.prompt('whats your full name');
+console.log(fullName);
+let firstTimeDriving= window.prompt('is this your first time driving');
+console.log(firstTimeDriving);
+let driversAge= window.prompt('how old are you');
+console.log(driversAge);
+if(driversAge>=17);
+return console.log("sorry");
+else
+return console.log("welcome"); 
+   // console.log('sorry unable to drive at the moment')
+    //else()
