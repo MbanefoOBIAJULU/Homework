3a902af55fff49b4fe85f812c3084726bea65562
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-03-25 18:08:23 +0100

    .

---
+++ chuka.js
@@ -0,0 +1,10 @@
+let customerName = window.prompt('whats your name?')
+let firstTimeDriving = window.prompt('is this your first time driving?')
+let customerNickName = window.prompt('whats your nickname?')
+let customerAge = window.prompt('how old are you?')
+ if (customerAge<=18){
+    console.log('sorry, unable to drive at the moment')
+ }
+else {
+    console.log('congratulations, you can drive now')
+}
