da6d37685676f37c9a22983fff32eb1f3504e027
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-03-27 14:19:38 +0100

    .

--- home.js
+++ home.js
@@ -36,9 +36,9 @@ fullPrice = (price + discount);
 console.log(fullPrice)
  */
 let i = 100;
-while   (i<=200)
-    console.log(i){
-    i += 20
+while   (i<=300)
+   { console.log(i) 
+        i += 40
 }
 
 let income =1500
