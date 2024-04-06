d654d8e35f99c4a0888fe0598ea03a7fd58904cc
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-04-06 20:08:48 +0100

    .

--- chuka.js
+++ chuka.js
@@ -10,7 +10,9 @@ else {
 }
 let uloma; 
 console.log(uloma) 
-*/
+
+
+
 const myText = document.getElementById("myText");
 const mySubmit = document.getElementById("mySubmit");
 const resultElement = document.getElementById("resultElement");
@@ -29,7 +31,15 @@ if (age >=100){
      }
      else if (age<0){
         resultElement.textContent = `your age can't be below 0`;
-    else{
+     else{
             resultElement.textContent = `your agemust be 18+ to enter this site`;
      }
-    } 
+    } */
+
+    for (var i = 1; i < 101; i++) {
+      if (i % 15 == 0) console.log("FizzBuzz");
+      else if (i % 3 == 0) console.log("Fizz");
+      else if (i % 5 == 0) console.log("Buzz");
+      else console.log(i);
+  }
+    
