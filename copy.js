a4622dfe4904d0f96fe38539fd6bbd26d09822ae
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-04-06 21:39:17 +0100

    .

--- copy.js
+++ copy.js
@@ -4,9 +4,11 @@ console.log(typeof(foo));
 
 let fuu = '32'
 console.log(fuu)
-console.log(typeof(fuu))*/
+console.log(typeof(fuu))
 
 let uloma = `${32 + 32}`
 console.log(typeof(uloma))
 console.log(uloma)
+*/
 
+ 
